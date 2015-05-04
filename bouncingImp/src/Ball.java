8de535faa46cbfe1521115c880fa0d4dbebf4131
krabban91 <anderssongabriel665@gmail.com>
2015-05-04 16:48:28 +0200

    derpningsdags.

--- bouncingImp/src/Ball.java
+++ bouncingImp/src/Ball.java
@@ -1,5 +1,6 @@
 import java.awt.*;
 import java.awt.geom.Ellipse2D;
+import java.awt.geom.Point2D;
 import java.util.LinkedList;
 
 /**
@@ -9,7 +10,7 @@ public class Ball {
     public static double areaWidth, areaHeight;
 
     public double x, y, vx, vy, r;
-
+    public boolean collided = false;
     public Ball(double x, double y, double vx, double vy, double r){
         this.x = x;
         this.y = y;
@@ -18,9 +19,48 @@ public class Ball {
         this.r = r;
     }
 
-    public void tick(double deltaT, LinkedList<Ball> balls){
-        // TODO: Collision check on all balls.
-        //
+    public void tick(double deltaT, LinkedList<Ball> collidingBalls){
+
+            for(Ball b : collidingBalls){
+                if(!this.collided){
+                // TODO: Collision check on all balls.
+                //
+                double dx = this.x-b.x;
+                double dy = this.y-b.y;
+                double hyp= Math.sqrt(dy * dy + dx * dx);
+                double angle = Math.asin(dx/hyp);
+
+                    Point2D.Double pol = this.rectToPolar(vx,vy);
+                Point2D.Double post = this.polarToRect(-pol.getX(),-pol.getY()+angle);
+
+                double mThis = 4*Math.PI*Math.pow(this.r,3)/3;
+                double mb = 4*Math.PI*Math.pow(b.r,3)/3;
+                //double I = mThis*this.vsomething;
+                double R;
+                //derping
+//                double xx = b.vx;
+//                double yy = b.vy;
+//                b.vx = this.vx;
+//                b.vy = this.vy;
+//                this.vx = xx;
+//                this.vy = yy;
+                    pol = this.rectToPolar(vx,vy);
+                    post = this.polarToRect(-pol.getX(),-(pol.getY()- angle));
+                    this.vx = post.getX();
+                    this.vy = post.getY();
+                    pol = this.rectToPolar(b.vx, b.vy);
+                    post = this.polarToRect(-pol.getX(), (-pol.getY() - angle));
+                    b.vx = post.getX();
+                    b.vy = post.getY();
+
+                    b.collided = true;
+            } else{
+                    this.collided = false;
+                }
+//                this.collided = !this.collided;
+//                b.collided = !b.collided;
+        }
+
 
         this.vy -= 9.82*deltaT;
         if (this.x < this.r || this.x > Ball.areaWidth - this.r) {
@@ -36,5 +76,19 @@ public class Ball {
 
     public Ellipse2D getGraphics(){
         return new Ellipse2D.Double(x - r, y - r, 2 * r, 2 * r);
+    }
+
+    public Point2D.Double polarToRect(double vel, double angle){
+        //vx and vy.
+        double x = Math.sin(angle)*vel;
+        double y = Math.cos(angle)*vel;
+        return new Point2D.Double(x,y);
+    }
+
+    public Point2D.Double rectToPolar(double x, double y){
+        double v = Math.sqrt(x*x+y*y);
+        double angle = Math.asin(x/v);
+        //velocity and angle
+        return new Point2D.Double(v,angle);
     }
 }
--- bouncingImp/src/DummyModel.java
+++ bouncingImp/src/DummyModel.java
@@ -1,3 +1,4 @@
+import javax.lang.model.type.IntersectionType;
 import java.awt.geom.Ellipse2D;
 import java.util.LinkedList;
 import java.util.List;
@@ -9,24 +10,40 @@ public class DummyModel implements IBouncingBallsModel {
 	public DummyModel(double width, double height) {
         Ball.areaWidth = width;
         Ball.areaHeight = height;
-        balls.add(new Ball(width/2,height/2,2,2,1));
-        balls.add(new Ball(2.7,2.7,-2,2,1));
+
+        //balls.add(new Ball(width/2,height/2,2,2,1));
+        //balls.add(new Ball(2.7,2.7,-2,2,1));
+        balls.add(new Ball(2,2,0,0,1));
+        balls.add(new Ball(2,6,0,1,1));
 	}
 
 	@Override
 	public void tick(double deltaT) {
         for(Ball b : balls){
-            b.tick(deltaT, balls);
+            LinkedList<Ball> collidingBalls = new LinkedList<Ball>();
+            for(Ball o : balls){
+                if(o != b && colliding(o,b)){
+                    collidingBalls.add(o);
+                }
+            }
+            b.tick(deltaT, collidingBalls);
         }
     }
 
 	@Override
 	public List<Ellipse2D> getBalls() {
+
         List<Ellipse2D> myBalls = new LinkedList<Ellipse2D>();
         for(Ball b : balls){
-            // TODO: only send in the balls that are colliding. "the touch balls"
             myBalls.add(b.getGraphics());
         }
 		return myBalls;
 	}
+
+    private boolean colliding(Ball fst, Ball snd){
+        double x = Math.abs(fst.x - snd.x);
+        double y = Math.abs(fst.y - snd.y);
+        double dist = Math.sqrt(x*x + y*y);
+        return dist <= (fst.r + snd.r);
+    }
 }
